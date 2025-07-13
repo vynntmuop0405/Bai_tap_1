1fe76eb6f63a3708e9aaa5f5173899c962762b01
VyNguyen <nguyenngocthaovy451999@gmail.com>
2025-07-13 23:17:13 +0700

    tét

---
+++ BT1.sql
@@ -0,0 +1,13 @@
+SELECT 
+  c.customer_id,
+  c.name,
+  SUM(oi.quantity * oi.price) AS total_spent,
+  CASE 
+    WHEN total_spent >= 1000000 THEN 'VIP'
+    WHEN total_spent >= 500000 THEN 'Potential'
+    ELSE 'Regular'
+  END AS customer_type
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+GROUP BY c.customer_id, c.name;
