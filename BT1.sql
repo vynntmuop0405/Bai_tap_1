47c3500df8dbc7b69cf8c5eb876d85ec0165cbcf
VyNguyen <nguyenngocthaovy451999@gmail.com>
2025-07-15 16:57:05 +0700

    change

--- BT1.sql
+++ BT1.sql
@@ -1,13 +1,29 @@
-SELECT 
-  c.customer_id,
-  c.name,
-  SUM(oi.quantity * oi.price) AS total_spent,
-  CASE 
-    WHEN total_spent >= 1000000 THEN 'VIP'
-    WHEN total_spent >= 500000 THEN 'Potential'
-    ELSE 'Regular' dsdsdsdsd
-  END AS customer_type
-FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN order_items oi ON o.order_id = oi.order_id
-GROUP BY c.customer_id, c.name;
+--SELECT * FROM batch_data WHERE batch_id = 'BATCH_02020';
+--SELECT * FROM inventory_data WHERE SKU = 'SKU_0001';
+--
+WITH 
+sorted_batch AS (
+	SELECT bd.batch_id, bd.SKU, bd.batch_quantity, bd.batch_received_date,
+		   ROW_NUMBER() OVER(PARTITION BY bd.SKU ORDER BY bd.batch_received_date asc, bd.batch_id) batch_order
+	FROM batch_data bd
+),
+product_quantity_accumulated AS (
+	SELECT sb.*,
+	SUM(sb.batch_quantity) OVER(PARTITION BY sb.SKU ORDER BY sb.batch_order
+	ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_sum --- Tính lũy kế số lượng hàng theo từng batch nhập
+	FROM sorted_batch sb
+),
+--SELECT * FROM product_quantity_accumulatedA
+inventory_with_batch AS (
+	SELECT p.*,
+		   i.Current_Stock,
+		   CASE WHEN p.running_sum <= i.Current_Stock THEN i.Current_Stock
+		        WHEN p.running_sum - p.batch_quantity < i.Current_Stock THEN i.Current_Stock - (p.running_sum - p.batch_quantity)
+				ELSE 0
+				END AS quantity_left_batch
+	FROM product_quantity_accumulated p
+	LEFT JOIN inventory_data i ON p.SKU = i.SKU
+)
+SELECT SKU, batch_id, Batch_Received_Date, quantity_left_batch
+FROM inventory_with_batch
+WHERE quantity_left_batch >0
